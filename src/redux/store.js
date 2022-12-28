5ec52a07c046cb4ec49165cf125b5f4f486e4c35
Sankar <sankar.yalamandla@tigeranalytics.com>
2022-12-28 11:47:11 +0530

    added thunks

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,9 @@ import { totalReducers } from "./reducers/combiners";
 
 export const store = createStore(
     totalReducers,
-    // compose(  
-        applyMiddleware(thunk)
-        // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(  
+        applyMiddleware(thunk),
+         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
  
-    // )  
+    )  
 )
